ead37a31ab3088aed21c7897ccf52b98231c25ea
Christian Fernandez <cfernandezlinux@gmail.com>
2014-07-29 23:20:07 -0400

    added script to clean scripts from confidential data

---
+++ clean_script.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# script to remove password and ip from other ruby scripts
+# before pushing to public repositories
+# in the future add this into arrays to do a better job.
+
+require 'pathname'
+
+@password = "password goes here"
+@ip = "ip goes here"
+@directory = "logoworks.com"
+
+Dir.glob("#{ @directory }/*.rb").each do |rubyFile|
+  singleRubyFile = Pathname.new(rubyFile).relative_path_from( Pathname.new("#{ @directory }") ).to_s
+  data = File.read("#{ rubyFile }")
+       	puts "Doing #{ rubyFile }"
+  newData =  data.gsub("#{ @password }", "********") 
+  newData =  newData.gsub("#{ @ip }","xx.xx.xx.xx")
+  File.open("scripts-sysadmin/#{ singleRubyFile }", "w") do | newFile |
+	  newFile.write(newData)
+  end
+end
